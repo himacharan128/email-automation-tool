14f1fce5437621a6da67ced1cc0e0c5a4eaa393b
HIMA CHARAN GANGULA <himacharan128@gmail.com>
2024-06-28 20:32:27 +0530

    Fix : secrets

---
+++ server/src/config/outlookConfig.ts
@@ -0,0 +1,10 @@
+import passport from 'passport';
+import { Strategy as OutlookStrategy } from 'passport-outlook';
+
+passport.use(new OutlookStrategy({
+  clientID: process.env.OUTLOOK_CLIENT_ID,
+  clientSecret: process.env.OUTLOOK_CLIENT_SECRET,
+  callbackURL: process.env.OUTLOOK_REDIRECT_URI
+}, (accessToken, refreshToken, profile, done) => {
+  return done(null, profile);
+}));
